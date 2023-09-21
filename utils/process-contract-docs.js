e0b8a5d0405165ed1cbc2b2706640db2686044de
robriks <marsterlund@gmail.com>
2023-09-21 16:18:19 -0400

    :hammer: Write nodeJS script to process smart contract technical docs into a 'stored-changes' file, to be used for updating mint.json

---
+++ utils/process-contract-docs.js
@@ -0,0 +1,41 @@
+const fs = require('fs');
+const path = require('path');
+
+function renameFilesAndRecordChanges(directoryPath) {
+  const changeLog = [];
+
+  function processDirectory(dirPath) {
+    const items = fs.readdirSync(dirPath);
+
+    for (const item of items) {
+      const itemPath = path.join(dirPath, item);
+      const isDirectory = fs.statSync(itemPath).isDirectory();
+
+      if (isDirectory) {
+        processDirectory(itemPath);
+      } else if (item.endsWith('.md') && !item.startsWith('README')) {
+        const newItemName = item.replace(/\.md$/, '.mdx');
+        const newPath = path.join(dirPath, newItemName);
+
+        fs.renameSync(itemPath, newPath);
+
+        // changeLog.push(`Renamed: ${itemPath} => ${newPath}`); // verbose option
+        changeLog.push(`${newPath}`);
+      }
+    }
+  }
+
+  processDirectory(directoryPath);
+
+  if (changeLog.length > 0) {
+    const logFile = path.join(directoryPath, 'stored-changes.md');
+    fs.writeFileSync(logFile, changeLog.join('\n'));
+    console.log(`Changes recorded in ${logFile}`);
+  } else {
+    console.log('No .md files found to rename.');
+  }
+}
+
+// Hardcoded dir param: if using this in a different dir, change this value
+const directoryPath = 'src';
+renameFilesAndRecordChanges(directoryPath);
